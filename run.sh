aa842bfb9369d9e0aba819be2abcb03b251a68ae
TBDavid2019 <tbdavid2019@gmail.com>
2025-04-16 19:35:46 +0800

    加上 GBDT/ Basic Neural Network 等功能

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 
-cd /home/ubuntu/underdog
-source /home/ubuntu/underdog/myenv/bin/activate
+cd /home/ec2-user/stock-underdog-ml
+source /home/ec2-user/stock-underdog-ml/myenv/bin/activate
 pip install -r requirements.txt
 python app.py  > /tmp/underdog123.log 2>&1
 deactivate
